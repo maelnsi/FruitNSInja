4a9f4db8a1a533a6387b9743ab8dcce1faa8289e
MaelStudio <maedef@claudel.org>
2024-12-07 14:20:28 -0500

    Object oriented

--- main.py
+++ main.py
@@ -2,61 +2,77 @@ import cv2
 import mediapipe as mp
 import pygame
 import numpy as np
+from fruit import Fruit
 
+class Game:
+    def __init__(self, screen):
+        self.screen = screen
+        self.running = True
+        self.dt = 0
+        self.clock = pygame.time.Clock()
+
+        self.fruit = Fruit(screen)
+
+    def handling_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def hand_tracking(self):
+        self.hand1 = None
+        self.hand2 = None
+
+        r, self.frame = capture.read()
+        spotted_hands = hands.process(self.frame).multi_hand_landmarks
+        if spotted_hands:
+            self.hand1 = spotted_hands[0]
+            if len(spotted_hands) > 1:
+                self.hand2 = spotted_hands[1]
+    
+    def update(self):
+        self.hand_tracking()
+
+        # Move fruit
+        self.fruit.move(self.dt)
+
+    def display(self):
+        # Draw hands on camera frame
+        if self.hand1:
+            mp_drawing.draw_landmarks(self.frame, self.hand1, mp_hands.HAND_CONNECTIONS)
+        if self.hand2:
+            mp_drawing.draw_landmarks(self.frame, self.hand2, mp_hands.HAND_CONNECTIONS)
+
+        # Convert OpenCV camera frame to Pygame image
+        self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+        self.frame = np.rot90(self.frame)
+        self.frame = pygame.surfarray.make_surface(self.frame)
+
+        # Display camera frame with hands
+        self.screen.blit(self.frame, (0, 0))
+
+        self.fruit.draw(self.screen)
+        pygame.display.flip()
+
+    def run(self):
+        while self.running:
+            self.handling_events()
+            self.update()
+            self.display()
+            self.dt = self.clock.tick(30) / 1000
+
+# Hand tracking with mediapipe
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
+capture = cv2.VideoCapture(0)
 
+# Pygame
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("Fruit NSInja")
-clock = pygame.time.Clock()
 
-apple_img = pygame.image.load("img/fruits/apple.png").convert_alpha()
-apple_img = pygame.transform.scale(apple_img, (100, 100))
-
-banana_xy = [300, 480]
-banana_vel = 40
-
-gravity = 2
-
-capture = cv2.VideoCapture(0)
-running = True
-
-while running:
-    r, frame = capture.read()
-    
-    results = hands.process(frame)
-    
-    hand1 = None 
-    hand2 = None
-    
-    if results.multi_hand_landmarks:
-        hand1 = results.multi_hand_landmarks[0]
-        if len(results.multi_hand_landmarks) > 1:
-            hand2 = results.multi_hand_landmarks[1]
-    
-    if(hand1):
-        mp_drawing.draw_landmarks(frame, hand1, mp_hands.HAND_CONNECTIONS)
-    if(hand2):
-        mp_drawing.draw_landmarks(frame, hand2, mp_hands.HAND_CONNECTIONS)
-    
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = np.rot90(frame)
-    frame = pygame.surfarray.make_surface(frame)
-    
-    screen.blit(frame, (0, 0))
-    screen.blit(apple_img, banana_xy)
-    pygame.display.update()
-    
-    banana_xy[1] -= banana_vel
-    banana_vel -= gravity
-    
-    clock.tick(30)
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+game = Game(screen)
+game.run()
 
 pygame.quit()
 cv2.destroyAllWindows()
