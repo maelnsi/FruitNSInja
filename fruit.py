5e0ea454918abb79553f89e5576b46eb39e58653
alimashhourr <alimas@claudel.org>
2025-01-09 12:15:44 -0500

    Fruit sound

--- fruit.py
+++ fruit.py
@@ -7,14 +7,13 @@ class Fruit(Sliceable):
         # Pick random fruit
         fruit_names = ["apple", "banana", "blueberry", "pineapple", "strawberry", "watermelon"]
         self.name = choice(fruit_names)
-
         super().__init__(screen, f"assets/fruits/{self.name}.png")
-        
+        self.sound = pygame.mixer.Sound("slice.mp3")
         print(self.name, self.rect.x, self.velocity[0], self.velocity[1])
     
     def slice(self):
         self.sliced = True
-
+        pygame.mixer.Sound.play(self.sound)
         # Change image
         self.image = pygame.image.load(f"assets/fruits/{self.name}_sliced.png")
         self.image = self.resize_image(self.image, 80)
